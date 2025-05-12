6def895d2cbb4e9dd78b7b71cb979b20667d349d
Duc Long Hoang <duclonghoang.dlh@gmail.com>
2025-05-12 12:40:26 +0200

    stability dzi image

--- src/lib/schemas.ts
+++ src/lib/schemas.ts
@@ -25,12 +25,12 @@ export type BucketConfig = z.infer<typeof bucketConfigSchema>
 export type DziVisConfig = z.infer<typeof dziVisConfigSchema>
 
 export const initialDziVisConfig: DziVisConfig = {
-	height: 20000,
+	height: 60000,
 	width: 60000,
 	tileSize: 254,
 	tileOverlap: 1,
-	xMin: 3.5,
-	xMax: 4.0,
+	xMin: 0,
+	xMax: 1,
 	yMin: 0,
 	yMax: 1,
 	precision: 15,
@@ -42,6 +42,6 @@ export const initialBucketConfig: BucketConfig = {
 	domain: "https://s3.cl2.du.cesnet.cz",
 	tenant: "1220acb0_52c1_4c07_8198_28e9c41e31ee",
 	name: "test-dzi",
-	imageFolder: "log_png_files",
-	imageFormat: "png",
+	imageFolder: "stabilita-na-003_webp_files",
+	imageFormat: "webp",
 }
